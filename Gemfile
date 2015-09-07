39eb943e77e41ccfd46c450d939943da74246189
Lukyanov Fedor <lukyanov.f.ua@gmail.com>
2015-09-07 14:36:58 +0300

    initial

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.4'
+gem 'pg'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jbuilder', '~> 2.0'
+gem 'devise_token_auth'
+gem 'omniauth-facebook'
+gem 'cancancan'
+gem 'carrierwave'
+gem 'kaminari'
+gem 'aasm'
+gem 'wicked'
+gem 'twitter-bootstrap-rails'
+
+group :development, :test do
+  gem 'byebug'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'faker'
+  gem 'database_cleaner'
+  gem 'shoulda-matchers'
+end
+
+group :test do
+  gem 'fuubar'
+  gem 'codeclimate-test-reporter'
+end
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,34 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'spec_helper'
+require 'rspec/rails'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.infer_spec_type_from_file_location!
+end
