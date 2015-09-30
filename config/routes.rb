72bc020d3a6ab8cad6f036cff06df7e3f94493b6
Lukyanov Fedor <lukyanov.f.ua@gmail.com>
2015-09-30 17:40:42 +0300

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    omniauth_callbacks: 'auth/omniauth_callbacks',
+  }
+
   scope except: %i(new edit), shallow: true do
     resources :projects do
       resources :tasks do
@@ -15,10 +19,6 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    omniauth_callbacks: 'auth/omniauth_callbacks',
-  }
 
   match '*unmatched_route.json', to: 'application#raise_not_found!', via: :all
 end
